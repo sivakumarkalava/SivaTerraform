1fd70f1779ea5240460dbfa340d7c51c559e1002
SivaLearnZ <100773216+sivakumarkalava@users.noreply.github.com>
2026-07-08 18:11:51 +0530

    Update bucket.tf

--- CreatingS3bucket/bucket.tf
+++ CreatingS3bucket/bucket.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "s3bucket4statefile09"
-}
+  bucket = "s3bucket4statefile09" 
+} ##Created bucket
 
 resource "aws_s3_bucket_versioning" "version" {
   bucket = aws_s3_bucket.mybucket.id
 
   versioning_configuration {
-    status = "Enabled"
+    status = "Enabled" ##added versioning and status set to enable
   }
 }
