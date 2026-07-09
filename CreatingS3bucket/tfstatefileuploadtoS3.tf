30b8672a613bc3aabb39938d8b6f68d2f6b7ebe7
SivaLearnZ <100773216+sivakumarkalava@users.noreply.github.com>
2026-07-09 11:01:07 +0530

    Enhance comments in S3 backend configuration
    Updated comments for clarity and added explanation about state locking.

--- CreatingS3bucket/tfstatefileuploadtoS3.tf
+++ CreatingS3bucket/tfstatefileuploadtoS3.tf
@@ -3,8 +3,9 @@
 ##Also enabled lock for state file so that two users cannot update the resources at the same time, similar to git merge conflicts. if we use state locks so that anyone applies the recent update (terraform apply) 
 ##if any other person also uses same command then that user get one error message saying the other user is trying to update the tfstate file. if that change is applied then the lock is disabled, then the second user 
 ##can apply his changes.
+
 terraform {
-  backend "s3" {
+  backend "s3" { ##we call this as backend file
     bucket       = "s3bucket4statefile09"
     key          = "prod/terraform.tfstate"
     region       = "us-east-1"
