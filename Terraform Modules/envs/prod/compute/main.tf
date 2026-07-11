33b6cbeda4ce8ae2353e98ca6c8632f99f85df6b
SivaLearnZ <100773216+sivakumarkalava@users.noreply.github.com>
2026-07-11 16:24:37 +0530

    Fix formatting of tags in aws_instance resource

--- Terraform Modules/envs/prod/compute/main.tf
+++ Terraform Modules/envs/prod/compute/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myserver" {
-tags = {
-Name = var.iname
-}
-ami = var.ami_id
-instance_type = var.itype
+  tags = {
+    Name = var.iname
+  }
+  ami           = var.ami_id
+  instance_type = var.itype
 }
