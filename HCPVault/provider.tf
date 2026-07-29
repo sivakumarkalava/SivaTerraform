fa0748caf4e26685b4ea25372ee6fbcd4a13f1ad
SivaLearnZ <100773216+sivakumarkalava@users.noreply.github.com>
2026-07-29 16:24:10 +0530

    Create provider.tf

---
+++ HCPVault/provider.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = data.vault_generic_secret.aws.data["access_key"]
+  secret_key = data.vault_generic_secret.aws.data["secret_key"]
+}
+data "vault_generic_secret" "aws" {
+  path = "secret/aws"
+}
+provider "vault" {
+  address = "http://13.218.139.119:8200/"
+  token   = var.mytoken
+}
+variable "mytoken" {
+  type      = string
+  sensitive = true
+}
