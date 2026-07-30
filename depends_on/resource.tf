25f1c84158dd5a586642fcf3eb36ab01344ddef5
SivaLearnZ <100773216+sivakumarkalava@users.noreply.github.com>
2026-07-30 12:23:08 +0530

    Update comments in resource.tf for clarity
    Comment out the example command for parallelism and clarify notes on resource creation.

--- depends_on/resource.tf
+++ depends_on/resource.tf
@@ -14,6 +14,6 @@ resource "aws_s3_bucket" "mybucket" {
 
 //Note: depends_on is nothing but creating resources one after another rather than creating parallelly. If we don;t specify depends_on all resources will create at a time parallely. 
 //Another point: By default terraform can create 10 resouces paralelly, and we cannot say what is the maximum limit cause there is no hard limit. if we want to specify how many we want to create we can say as 
-terraform apply --paralellism=20 this number can change according to our needs.
+//terraform apply --paralellism=20 this number can change according to our needs.
 //And this depends_on is explicit giving cause we are telling terraform to create like that but by default it has implicit creation, this will apply for vpc, subnets, Route tables, internet gateway, nat gateway,
-subnet association and etc..,
+//subnet association and etc..,
